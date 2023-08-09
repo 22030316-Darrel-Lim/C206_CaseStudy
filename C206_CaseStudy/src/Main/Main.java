068abc7e588b7ff8a7d037c37b51b7d05a74a003
silen <silen@Darrel_Lim>
2023-08-09 14:40:36 +0800

    Setting up classes

---
+++ C206_CaseStudy/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
