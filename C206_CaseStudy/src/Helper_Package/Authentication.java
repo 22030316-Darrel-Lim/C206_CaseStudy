c30f62e10849def402fcb144d475fa7d9fc040d5
silen <silen@Darrel_Lim>
2023-08-10 23:17:57 +0800

    Pushing unfactored methods for vendor + admin

--- C206_CaseStudy/src/Helper_Package/Authentication.java
+++ C206_CaseStudy/src/Helper_Package/Authentication.java
@@ -3,11 +3,14 @@ package Helper_Package;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import Helper.DBUtil;
+
 public class Authentication {
   
-	public static DBData Login(String email, String password) {
-		DBData CREDENTIAL = new DBData(email, password);
-		
+	private static DBData CREDENTIAL;
+	
+	private static void checkCREDENTIAL() {
+		// Initialize the access and id after creating the DBData object
 		String CREDENTIAL_access = CREDENTIAL.getUser_access();
 		String CREDENTIAL_id = CREDENTIAL.getUser_id();
 		
@@ -15,51 +18,58 @@ public class Authentication {
 		if (CREDENTIAL == null || CREDENTIAL_id == null || CREDENTIAL_access == null || CREDENTIAL_id.isEmpty() || CREDENTIAL_access.isEmpty()) {
 			CREDENTIAL = null;
 		}
+	}
+	
+	public static DBData Login(String email, String password) {
+		CREDENTIAL = new DBData(email, password);
+		
+		checkCREDENTIAL();
 		
 		return CREDENTIAL;
 	}
 
 	private static DBData RegisterAccount(String name, String email, String password, String access, String[] otherInfo) {
-	    DBData CREDENTIAL = new DBData(name, email, password, access, otherInfo);
+		CREDENTIAL = new DBData(name, email, password, access, otherInfo);
 
-	    // Initialize the access and id after creating the DBData object
-	    String CREDENTIAL_access = CREDENTIAL.getUser_access();
-	    String CREDENTIAL_id = CREDENTIAL.getUser_id();
-
-	    // Return null if any of the 5 has error
-	    if (CREDENTIAL == null || CREDENTIAL_id == null || CREDENTIAL_access == null || CREDENTIAL_id.isEmpty() || CREDENTIAL_access.isEmpty()) {
-	        CREDENTIAL = null;
-	    }
+		checkCREDENTIAL();
 
 	    return CREDENTIAL;
 	}
-
 	
 	public static DBData RegisterAccountNormal(String name, String email, String password, String[] otherInfo) {
 		String access = "normal";
-		DBData CREDENTIAL = RegisterAccount(name, email, password, access, otherInfo);
+		
+		CREDENTIAL= RegisterAccount(name, email, password, access, otherInfo);
+		
 		return CREDENTIAL;
 	}
 	
 	public static DBData RegisterAccountVendor(String name, String email, String password, String[] otherInfo) {
 		String access = "vendor";
-		DBData CREDENTIAL = RegisterAccount(name, email, password, access, otherInfo);
+		
+		CREDENTIAL = RegisterAccount(name, email, password, access, otherInfo);
+		
 		return CREDENTIAL;
 	}
 	
 	public static DBData RegisterAccountAdmin(String name, String email, String password, String[] otherInfo) {
 		String access = "admin";
-		DBData CREDENTIAL = RegisterAccount(name, email, password, access, otherInfo);
+		
+		CREDENTIAL = RegisterAccount(name, email, password, access, otherInfo);
+		
 		return CREDENTIAL;
 	}
-	public static boolean CheckEmailDB(String email) {
-        boolean exists = false;
+	
+	public static Boolean CheckEmailDB(String email) {
+        Boolean exists = null;
 
         // Check if empty
         if (email == null || email.isEmpty()) {
             return exists;
         }
-
+        
+        exists = false;
+        
         try {
             DBUtil.init(DBData.JDBCURL, DBData.DBUSERNAME, DBData.DBPASSWORD);
 
@@ -80,14 +90,7 @@ public class Authentication {
 
         return exists;
     }
-	public static String getUserAccessType(String email, String password) {
-        DBData credentials = Login(email, password);
+	
 
-        if (credentials != null) {
-            return credentials.getUser_access();
-        } else {
-            return null;
-        }
-    }
 	
 } // End of Class
--- C206_CaseStudy/src/Not_In_Use/Main.java
+++ C206_CaseStudy/src/Not_In_Use/Main.java
@@ -16,13 +16,14 @@ public class Main {
 	}
 
 	// NOT IN USE - USE THE DEFAULT PACKAGE
+	// THIS SERVERS AS A WORKSTATION TO AVOID CONFLICT WITH THE DEFAULT CLASS
 
 	// Main Menu is displayed when application is open
 	public static void mainMenu() {
 		int i = 0;
 		while (i != 9) {
 			Helper.line(40, "-");
-			System.out.println("== SCHOOL LUNCH BOX MAIN MENU");
+			System.out.println("== SCHOOL LUNCH BOX MAIN MENU ==");
 			System.out.println("1) Login\n2) Register\n9)Exit");
 			i = Helper.readInt("Enter Option: ");
 
@@ -45,6 +46,9 @@ public class Main {
 
 					case "admin":
 						adminMenu();
+
+					default:
+						System.out.println("Error - Access Type not found");
 					}
 				}
 
@@ -82,6 +86,10 @@ public class Main {
 			case 9:
 				System.out.println("Thank you for using our system!");
 				break;
+
+			default:
+				System.out.println("Invlaid Option");
+
 			}
 		}
 	}
@@ -91,11 +99,13 @@ public class Main {
 		while (i != 9) {
 			Helper.line(40, "-");
 			System.out.println("== VENDOR MENU ==");
-			System.out.println("1)View All Menu \n2)Add new Menu \n3)Delete Menu \n4)Add new Item \n9)Exit");
+			System.out.println(
+					"1)View All Menu \n2)Add new Menu \n3)Delete Menu \n4)Add new Item \n5)Delete Items \n9)Exit");
 			i = Helper.readInt("Enter Option: ");
 			switch (i) {
 			case 1:
 				// Call method to see all menu
+				viewAllMenu();
 
 			case 2:
 				// New Menu + add Items
@@ -109,9 +119,15 @@ public class Main {
 				// Add new Item to the menu
 				addFoodItem();
 
+			case 5:
+				// Delete Item from Menu
+				deleteFoodItem();
 			case 9:
 				System.out.println("Thank you for using our system!");
 				break;
+
+			default:
+				System.out.println("Invlaid Option");
 			}
 		}
 	}
@@ -128,42 +144,157 @@ public class Main {
 			switch (i) {
 			case 1:
 				// Call method to view all User
+				viewAllUser();
 
 			case 2:
 				// View all School
+				viewAllSchool();
 
 			case 3:
 				// View all Menu
+				viewAllMenu();
 
 			case 4:
 				// View All Orders
+				viewAllOrder();
 
 			case 5:
 				// Call Method to Create User
+				createUser();
 
 			case 6:
 				// Delete User
+				deleteUser();
 
 			case 9:
 				System.out.println("Thank you for using our system!");
 				break;
+
+			default:
+				System.out.println("Invlaid Option");
 			}
 		}
 	}
 
+	// ==========================
+	// Methods For USER
+	// ==========================
+
+	private static void viewAllMenu() {
+		// TODO SQL to view all Menu
+	}
+
+	// ==========================
+	// Methods For VENDOR
+	// ==========================
+
 	// Method for Vendor to create a new food Menu
+
 	private static void createFoodMenu() {
-		System.out.println("== Create New Food Menu ==");
+		System.out.println("== CREATE NEW FOOD MENU ==");
+		// TODO Create a new Food menu - SQL to Insert new Menu
+		addFoodItem();
+
 	}
 
 	// Method for Vendor to delete the Food Menu
+
 	private static void deleteFoodMenu() {
-		System.out.println("");
+		System.out.println("== DELETE FOOD MENU ==");
+		// TODO Run SQL Statement to get Menu_id
+
+		int id = Helper.readInt("Select Menu_ID to delete: ");
+		// TODO Run SQL Statement to delete base on Menu_Id
 	}
 
-	// Method for Vendor to delete the Food Menu
+	// Method for Vendor to add new food
+
 	private static void addFoodItem() {
-		System.out.println("");
+		System.out.println("== ADD FOOD TO MENU ==");
+
+		// Looping for input
+		int i = 0;
+		while (i != 9) {
+			System.out.println("1) Add exisitng food to menu \n2) Create food and add it to menu \n9) Exit");
+			i = Helper.readInt("Enter Option: ");
+
+			switch (i) {
+			case "1":
+				// TODO Retrieve food from SQL and add it
+
+			case "2":
+				// Create new food and add it to SQL
+
+				String food = Helper.readString("Enter Food Name: ");
+				String description = Helper.readString("Enter Food Description: ");
+				String dietary = Helper.readString("Enter Food Dietary: ");
+				String ingredients = Helper.readString("Enter Food Ingredients: ");
+				Double price = Helper.readDouble("Enter Food Price: ");
+				int qty = Helper.readInt("Enter Food Quantity");
+				int menu_id = Helper.readInt("Enter Menu_id: ");
+
+				// TODO Run insert SQL Statement to create new food and add it to menu
+
+			case "9":
+				// Exit from Menu Option
+				break;
+
+			default:
+				// Error message
+				System.out.println("Invalid choice. Please choose again.");
+			}
+		}
 	}
 
+	// Method for Vendor to delete Food
+	private static void deleteFoodItem() {
+		System.out.println("== DELETE FOOD ITEM ==");
+		// Method to view the Food
+		// SQL
+		String food = Helper.readString("Enter Food Name: ");
+		String confirm = Helper.readString("Confirm Deletetion? (y/n): ");
+		if (confirm.equalsIgnoreCase("y")) {
+			// TODO Run Delete SQL
+		} else {
+			System.out.println("Deletation Aborted");
+		}
+	}
+
+	// ==========================
+	// Methods For ADMIN
+	// ==========================
+
+	private static void viewAllUser() {
+		// TODO SQL Code to view all Users
+
+	}
+
+	private static void viewAllSchool() {
+		// TODO SQL Code to view all Schools
+
+	}
+
+	private static void viewAllOrder() {
+		// TODO SQL Code to view all Orders
+
+	}
+
+	private static void createUser() {
+		// TODO SQL Code to create any user
+		String email = Helper.readString("Enter Email: ");
+		String name = Helper.readString("Enter Name: ");
+		String password = Helper.readString("Enter Password: ");
+		String access = Helper.readString("Enter Access Type: ");
+	}
+
+	private static void deleteUser() {
+		// TODO SQL Code to delete Users + View All Users
+		String email = Helper.readString("Enter Email: ");
+		String confirm = Helper.readString("Confirm Deletetion? (y/n): ");
+		if (confirm.equalsIgnoreCase("y")) {
+			// TODO Run Delete SQL
+		} else {
+			System.out.println("Deletation Aborted");
+		}
+	}
 }
